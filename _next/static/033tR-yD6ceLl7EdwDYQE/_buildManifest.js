8284098fe03f6bd01ade4b3aef9f42b8a7090b2b
Richoivan <Richoivan@users.noreply.github.com>
2026-06-04 13:04:12 +0000

    deploy: 8c4a800520b9fa057bc83bc0a81a95a879a16a88

---
+++ _next/static/033tR-yD6ceLl7EdwDYQE/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Nereaperfumery//_next/:path+",
+        "destination": "/Nereaperfumery/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
